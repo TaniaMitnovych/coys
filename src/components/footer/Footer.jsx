97bd8901a60f56c0f089cfa06d40f9c7afbc2c4b
TaniaMitnovych <taniamitnovych15@gmail.com>
2022-09-14 18:02:35 +0300

    created footer component and its styles

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import instagramLogo from '../../logo/instagram_logo.svg';
+import facebookLogo from '../../logo/facebook_logo.svg';
+import arrowTop from '../../logo/arrow_top.svg';
+import './Footer.css';
+
+const Footer = () => {
+  const {t}=useTranslation();
+  return (
+    <div className='footer'>
+      <ul className='footer__social-list'>
+        <li className='footer__social-item'><a href="https://www.instagram.com/coys_brand/"><img src={instagramLogo}/></a></li>
+        <li className='footer__social-item'><a href="https://www.facebook.com/coysukraine/"><img src={facebookLogo}/></a></li>
+      </ul>
+
+      <ul className='footer__usefull-links-list'>
+        <li className='footer__usefull-links-item'><a href=''>{t('maintenance')}</a></li>
+        <li className='footer__usefull-links-item'><a href=''>{t('aboutBrand')}</a></li>
+        <li className='footer__usefull-links-item'><a href=''>{t('lookbooks')}</a></li>
+      </ul>
+
+      <a className='footer__to-top-button' href="#header"><img src={arrowTop}/></a>
+    </div>
+  );
+};
+
+export default Footer;
