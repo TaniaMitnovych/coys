b0aab689b51a992e9877a52c8d7440d9901245f5
TaniaMitnovych <taniamitnovych15@gmail.com>
2022-12-23 20:55:08 +0200

    created main page banner

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../../logo/coys_logo_white.svg';
+import './Banner.css';
+import { useTranslation } from 'react-i18next';
+const Banner = () => {
+  const {t}=useTranslation();
+  return (
+    <div className='banner'>
+      <h1 className='banner__header'>Colors Of Your Soul</h1>
+      <p className='banner__text'>{t('about')}</p>
+      <img className="banner__logo" src={logo} alt="logo"/>
+    </div>
+  );
+};
+
+export default Banner;
