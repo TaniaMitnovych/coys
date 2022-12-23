1e65465fd64575ffdb091f2201a6c0d8f7ff23ac
TaniaMitnovych <taniamitnovych15@gmail.com>
2022-12-23 15:03:46 +0200

    added main page component

---
+++ src/components/mainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PhotoSliderPC from '../photoSlider/PhotoSlider';
+
+const MainPage = () => {
+  return (
+    <>
+      <PhotoSliderPC/>
+    </>
+  );
+};
+
+export default MainPage;
