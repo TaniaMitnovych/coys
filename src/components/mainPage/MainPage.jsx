ae86d4345625622bd9f5568ef8abf5cfdb06d164
TaniaMitnovych <taniamitnovych15@gmail.com>
2022-12-23 20:55:54 +0200

    added banner to the MainPage component

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Banner from '../banner/Banner';
 import PhotoSliderPC from '../photoSlider/PhotoSlider';
-
+import './MainPage.css';
 const MainPage = () => {
   return (
-    <>
+    <div className='mainpage'>
       <PhotoSliderPC/>
-    </>
+      <Banner/>
+    </div>
   );
 };
 
