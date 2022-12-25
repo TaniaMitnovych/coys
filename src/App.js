92de79235388813c240074b9c0a0e3ef7b281636
TaniaMitnovych <taniamitnovych15@gmail.com>
2022-12-25 20:30:24 +0200

    implemented routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next'
+import BrandName from './components/brandName/BrandName';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import NavList from './components/navList/NavList';
+import './reset.css';
 import './App.css';
-
+import PhotoSliderPC from './components/photoSlider/PhotoSlider';
+import Layout from './components/layout/Layout';
+import { Routes,Route } from 'react-router-dom';
+import MainPage from './components/mainPage/MainPage';
+const languages=[
+  {value:'en', text: 'EN'},
+  {value:'uk', text: 'UK'}
+]
 function App() {
+  const {t}=useTranslation();
+  const [lang,setLang]=useState('en');
+
+  const handleLangChange=(event)=>{
+    setLang(event.target.value);
+    let location="http://localhost:3000/";
+    window.location.replace(location+'?lng='+event.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<MainPage/>}/>
+        </Route>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+/*
+sass ./src/components/brandName/BrandName.scss ./src/components/brandName/BrandName.css 
+*/
