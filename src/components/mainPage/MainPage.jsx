be54237a8f2d122d9f69aa96b42d58283bc0bee2
TaniaMitnovych <taniamitnovych15@gmail.com>
2022-12-25 19:50:23 +0200

    added individual order component to the main page

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import Banner from '../banner/Banner';
+import ImageMenu from '../imageMenu/ImageMenu';
 import PhotoSliderPC from '../photoSlider/PhotoSlider';
 import './MainPage.css';
+import man from '../../images/man.jpeg';
+import woman from '../../images/woman.png';
+import { useTranslation } from 'react-i18next';
+import IndividualOrder from '../individualOrder/IndividualOrder';
+
 const MainPage = () => {
+  const { t } = useTranslation();
+  let imageMenuItemsCategories = [{ link: "/woman", photo: woman, text: t("woman") },
+  { link: "/man", photo: man, text: t("man") }];
   return (
     <div className='mainpage'>
-      <PhotoSliderPC/>
-      <Banner/>
+      <PhotoSliderPC />
+      <Banner />
+      <ImageMenu
+        className="categories"
+        menuItems={imageMenuItemsCategories}
+      />
+      <IndividualOrder/>
     </div>
   );
 };
