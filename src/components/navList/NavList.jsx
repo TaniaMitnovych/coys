c584eedc7380200b701913a1ea6b7886609aeddb
TaniaMitnovych <taniamitnovych15@gmail.com>
2023-01-17 01:50:56 +0200

    added link to the navigation list

--- src/components/navList/NavList.jsx
+++ src/components/navList/NavList.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import './NavList.css';
+import { Link } from 'react-router-dom';
 const NavList = () => {
   const {t}=useTranslation();
   return (
       <ul className='nav-list'>
-        <li className="nav-list__item"><a>{t('woman')}</a></li>
+        <li className="nav-list__item">
+          <Link to="/woman">
+          {t('woman')}
+          </Link>
+          </li>
         <li className="nav-list__item"><a>{t('man')}</a></li>
         <li className="nav-list__item"><a>{t('contacts')}</a></li>
       </ul>
