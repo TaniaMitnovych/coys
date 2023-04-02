ece7b039fdac54267b47692b5afeb2fdc1d247d6
TaniaMitnovych <taniamitnovych15@gmail.com>
2023-04-03 00:02:58 +0300

    added props to the catalogue page

--- src/pages/cataloguePage/CataloguePage.jsx
+++ src/pages/cataloguePage/CataloguePage.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import Catalogue from '../../components/catalogue/Catalogue';
 import CatalogueMenu from '../../components/catalogueMenu/CatalogueMenu';
 import ItemCard from '../../components/itemCard/ItemCard';
-import forWomen from '../../women.json';
 import './CataloguePage.css';
-const CataloguePage = () => {
+const CataloguePage = (props) => {
   return (
     <div className='catalogue-page'>
       <CatalogueMenu/>
-      <Catalogue items={forWomen}/>
+      <Catalogue items={props.for}/>
     </div>
   );
 };
